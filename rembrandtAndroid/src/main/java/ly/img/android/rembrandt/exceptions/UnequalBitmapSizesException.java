bf802a68678f5af7488e6a582358d5a2a5d41875
Sandro Winkler <code@sandrowinkler.com>
2016-12-16 11:51:08 +0100

    rename bitmap dimension to bitmap size for better understanding

--- rembrandtAndroid/src/main/java/ly/img/android/rembrandt/exceptions/UnequalBitmapSizesException.java
+++ rembrandtAndroid/src/main/java/ly/img/android/rembrandt/exceptions/UnequalBitmapSizesException.java
@@ -6,10 +6,10 @@ import android.graphics.Bitmap;
  * Created by winklerrr on 16/12/2016.
  */
 
-public class UnequalBitmapDimensionsException extends RuntimeException {
+public class UnequalBitmapSizesException extends RuntimeException {
 
-    public UnequalBitmapDimensionsException(final Bitmap bitmap1, final Bitmap bitmap2) {
-        super("Dimensions unequal: "
+    public UnequalBitmapSizesException(final Bitmap bitmap1, final Bitmap bitmap2) {
+        super("Sizes unequal: "
                 + bitmap1.getHeight() + "x" + bitmap1.getWidth() + " vs. "
                 + bitmap2.getHeight() + "x" + bitmap2.getWidth());
     }
