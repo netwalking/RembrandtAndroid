faf9b74934ea5926e9e64f62fb71d332a0fd2b35
Sandro Winkler <code@sandrowinkler.com>
2017-01-11 13:50:51 +0100

    Remove unused parceable implementation from RembrandtCompareResult

--- rembrandtAndroid/src/main/java/ly/img/android/rembrandt/RembrandtComparisonResult.java
+++ rembrandtAndroid/src/main/java/ly/img/android/rembrandt/RembrandtComparisonResult.java
@@ -8,7 +8,7 @@ import android.os.Parcelable;
  * Created by winklerrr on 13/12/2016.
  */
 
-public class RembrandtComparisonResult implements Parcelable {
+public class RembrandtComparisonResult {
 
     private final long differentPixels;
     private final double percentageOfDifferentPixels;
@@ -38,36 +38,4 @@ public class RembrandtComparisonResult implements Parcelable {
         return bitmapsEqual;
     }
 
-    protected RembrandtComparisonResult(Parcel in) {
-        differentPixels = in.readLong();
-        percentageOfDifferentPixels = in.readDouble();
-        comparisionBitmap = (Bitmap) in.readValue(Bitmap.class.getClassLoader());
-        bitmapsEqual = in.readByte() != 0;
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeLong(differentPixels);
-        dest.writeDouble(percentageOfDifferentPixels);
-        dest.writeValue(comparisionBitmap);
-        dest.writeByte((byte) (bitmapsEqual ? 1 : 0));
-    }
-
-    @SuppressWarnings("unused")
-    public static final Parcelable.Creator<RembrandtComparisonResult> CREATOR = new Parcelable.Creator<RembrandtComparisonResult>() {
-        @Override
-        public RembrandtComparisonResult createFromParcel(Parcel in) {
-            return new RembrandtComparisonResult(in);
-        }
-
-        @Override
-        public RembrandtComparisonResult[] newArray(int size) {
-            return new RembrandtComparisonResult[size];
-        }
-    };
 }
