69951338b7db0d75cc4e2e78d3fd7224086c3e26
Sandro Winkler <code@sandrowinkler.com>
2017-01-12 11:26:04 +0100

    Update main activity with animations

--- app/src/main/java/ly/img/android/example/MainActivity.java
+++ app/src/main/java/ly/img/android/example/MainActivity.java
@@ -1,26 +1,36 @@
 package ly.img.android.example;
 
+import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import ly.img.android.rembrandt.Rembrandt;
 import ly.img.android.rembrandt.RembrandtComparisonResult;
 
 public class MainActivity extends Activity {
 
+    private TextView resultTextView;
+    private TextView resultInfoTextView;
+
     private LinearLayout layout;
     private ImageView imageView1;
     private ImageView imageView2;
     private ImageView comparisonImageView;
+
     private Button compareButton;
+    private Button separateButton;
+    private Animator currentAnimator;
 
     private Bitmap bitmap1;
     private Bitmap bitmap2;
@@ -43,11 +53,16 @@ public class MainActivity extends Activity {
     }
 
     private void initWidgets() {
+        resultTextView = (TextView) findViewById(R.id.activity_main_textview_result);
+        resultInfoTextView = (TextView) findViewById(R.id.activity_main_textview_result_info);
+
         layout = (LinearLayout) findViewById(R.id.activity_main_layout_imageviews);
         imageView1 = (ImageView) findViewById(R.id.activity_main_imageview_1);
         imageView2 = (ImageView) findViewById(R.id.activity_main_imageview_2);
         comparisonImageView = (ImageView) findViewById(R.id.activity_main_imageview_comparison);
+
         compareButton = (Button) findViewById(R.id.activity_main_button_compare);
+        separateButton = (Button) findViewById(R.id.activity_main_button_separate);
     }
 
     private void initBitmaps() {
@@ -64,17 +79,37 @@ public class MainActivity extends Activity {
     }
 
     private void addListeners() {
+        addCompareButtonOnClickListener();
+        addSeparateButtonOnClickListener();
+    }
+
+    private void addCompareButtonOnClickListener() {
         compareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                compareBitmapsAndShowResult();
+                onClickCompare();
             }
         });
     }
 
-    private void compareBitmapsAndShowResult() {
+    private void addSeparateButtonOnClickListener() {
+        separateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickSeparate();
+            }
+        });
+    }
+
+    private void onClickCompare() {
         compareBitmaps();
+        changeToSeparateButton();
         showResult();
+    }
+
+    private void onClickSeparate() {
+        animateSeparation();
+        changeToCompareButton();
     }
 
     private void compareBitmaps() {
@@ -82,30 +117,113 @@ public class MainActivity extends Activity {
     }
 
     private void showResult() {
+        animateComparison();
+        showTextResults();
+    }
+
+    private void changeToSeparateButton() {
+        compareButton.setVisibility(View.GONE);
+        separateButton.setVisibility(View.VISIBLE);
+    }
+
+    private void animateSeparation() {
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playSequentially(getFadeOutAnimator(), getMoveOutAnimator());
+
+        if (currentAnimator != null) currentAnimator.cancel();
+        animatorSet.start();
+    }
+
+    private void changeToCompareButton() {
+        separateButton.setVisibility(View.GONE);
+        compareButton.setVisibility(View.VISIBLE);
+    }
+
+    private void animateComparison() {
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(imageView1.getMeasuredWidth(), imageView1.getMeasuredHeight());
+        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        comparisonImageView.setLayoutParams(layoutParams);
+        comparisonImageView.invalidate();
         comparisonImageView.setImageBitmap(compareResult.getComparisionBitmap());
         comparisonImageView.setAlpha(0f);
         comparisonImageView.setVisibility(View.VISIBLE);
 
-        float parentY = layout.getY() + layout.getHeight() / 2;
-        float child1Y = imageView1.getY() + imageView1.getHeight() / 2;
-        float child2Y = imageView2.getY() + imageView2.getHeight() / 2;
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playSequentially(getMoveInAnimator(), getFadeInAnimator());
 
-        AnimatorSet moveAnimator = new AnimatorSet();
-        moveAnimator.playTogether(
-                ObjectAnimator.ofFloat(imageView1, "translationY", imageView1.getTranslationY(), parentY - child1Y),
-                ObjectAnimator.ofFloat(imageView2, "translationY", imageView2.getTranslationY(), parentY - child2Y)
-        );
-        moveAnimator.setDuration(1000);
-
-        AnimatorSet fadeAnimator = new AnimatorSet();
-        fadeAnimator.playTogether(
-                ObjectAnimator.ofFloat(comparisonImageView, "alpha", comparisonImageView.getAlpha(), 1)
-        );
-        fadeAnimator.setDuration(1000);
-
-        AnimatorSet sequence = new AnimatorSet();
-        sequence.playSequentially(moveAnimator, fadeAnimator);
-        sequence.start();
+        if (currentAnimator != null) currentAnimator.cancel();
+        currentAnimator = animatorSet;
+        animatorSet.start();
     }
 
+    private void showTextResults() {
+        updateResultTextView();
+        updateResultInfoTextView();
+    }
+
+    private AnimatorSet getMoveInAnimator() {
+        float deltaCenter = (layout.getHeight() - imageView2.getHeight()) / 2;
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(
+                ObjectAnimator.ofFloat(imageView1, "translationY", imageView1.getTranslationY(), deltaCenter),
+                ObjectAnimator.ofFloat(imageView2, "translationY", imageView2.getTranslationY(), -deltaCenter)
+        );
+        animatorSet.setDuration(1000);
+        return animatorSet;
+    }
+
+    private AnimatorSet getMoveOutAnimator() {
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(
+                ObjectAnimator.ofFloat(imageView1, "translationY", imageView1.getTranslationY(), 0),
+                ObjectAnimator.ofFloat(imageView2, "translationY", imageView2.getTranslationY(), 0)
+        );
+        animatorSet.setDuration(1000);
+        return animatorSet;
+    }
+
+    private AnimatorSet getFadeInAnimator() {
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(
+                ObjectAnimator.ofFloat(comparisonImageView, "alpha", comparisonImageView.getAlpha(), 1)
+        );
+
+        long alpha = (long) comparisonImageView.getAlpha() * 1000;
+        animatorSet.setDuration(1000 - alpha);
+        return animatorSet;
+    }
+
+    private AnimatorSet getFadeOutAnimator() {
+        AnimatorSet animator = new AnimatorSet();
+        animator.playTogether(
+                ObjectAnimator.ofFloat(comparisonImageView, "alpha", comparisonImageView.getAlpha(), 0)
+        );
+
+        long alpha = (long) comparisonImageView.getAlpha() * 1000;
+        animator.setDuration(alpha);
+        return animator;
+    }
+
+    private void updateResultTextView() {
+        String text;
+        int color;
+
+        if (compareResult.areBitmapsEqual()) {
+            text = getString(R.string.equality);
+            color = Color.GREEN;
+        } else {
+            text = getString(R.string.diversity);
+            color = Color.RED;
+        }
+
+        resultTextView.setText(text);
+        resultTextView.setTextColor(color);
+    }
+
+    private void updateResultInfoTextView() {
+        String differences = String.format(getString(R.string.differences), compareResult.getNumberOfDifferentPixels());
+        String percentageDifferences = String.format(getString(R.string.percentageDifferences), compareResult.getPercentageOfDifferentPixels());
+        resultInfoTextView.setText("(" + differences + ", " + percentageDifferences + ")");
+    }
 }
