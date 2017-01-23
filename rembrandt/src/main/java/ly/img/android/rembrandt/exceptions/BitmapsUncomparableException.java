05d3a0894ca993d08dbea5d53eb7b607f502f85f
Sandro Winkler <code@sandrowinkler.com>
2017-01-23 11:11:12 +0100

    Add new uncomparable exception for both cases of uncoparability for bitmaps

---
+++ rembrandt/src/main/java/ly/img/android/rembrandt/exceptions/BitmapsUncomparableException.java
@@ -0,0 +1,20 @@
+package ly.img.android.rembrandt.exceptions;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by winklerrr on 23/01/2017.
+ */
+
+public class BitmapsUncomparableException extends RuntimeException {
+    public BitmapsUncomparableException(final Bitmap bitmap1, final Bitmap bitmap2) {
+        super("Bitmap are uncomparable. Either their configs differ or their dimensions."
+                + "\nConfigs: "
+                + bitmap1.getConfig().toString() + " vs. "
+                + bitmap2.getConfig().toString()
+                + "\nDimensions: "
+                + bitmap1.getHeight() + "x" + bitmap1.getWidth() + " vs. "
+                + bitmap2.getHeight() + "x" + bitmap2.getWidth()
+        );
+    }
+}
